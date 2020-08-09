2e4642eaa0e3b1f80ae4c759761660b95a22daad
CHARLIE <jcarlosbotero1@outlook.com>
2020-08-09 02:53:40 -0500

    MIT license sync in all files

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,38 @@
-// ============================================
-// Script by: Juan Carlos Botero 
-// Date: 08/08/2020
-// Version: 2.0.0
-// ============================================
-//      Copyright (c) 2020 Charlie Projects
-//      github.com/CharlieProjects/MULTI-19
-// ============================================
-// ========     CONVEYOR MULTI-19      ========
-// ========  Multiplo Colombia S.A.S.  ========
-// ============================================
+/*
+ * ============================================
+ * Script by: Juan Carlos Botero 
+ * Date: 08/08/2020
+ * Version: 2.0.0
+ * ============================================
+ *   Copyright (c) 2020 Charlie Projects
+ *   github.com/CharlieProjects/MULTI-19
+ * ============================================
+ * ========     CONVEYOR MULTI-19      ========
+ * ========  Multiplo Colombia S.A.S.  ========
+ * ============================================
+ * 
+ *  MIT License
+ *
+ * Copyright (c) 2020 CHARLIE PROJECTS
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 
 #include <Arduino.h>
 #include <Config.h>
@@ -30,7 +54,7 @@ extern "C"
 #define E_STOP    4
 byte state = STAND_BY;
 
-void setup()
+void  setup()
 {
   /* start Display */
   display.init();
