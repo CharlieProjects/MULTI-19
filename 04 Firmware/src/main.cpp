ed4d692fca79a70fda8ebf57c8875098e503fb6f
Juan Carlos Botero <jcarlosbotero1@outlook.com>
2022-09-20 20:03:32 -0400

    ...

--- 04 Firmware/src/main.cpp
+++ 04 Firmware/src/main.cpp
@@ -1,125 +1,142 @@
-/* ==============================================================================
- * Script by: Juan Carlos Botero 
- * Date: 08/08/2020
- * Version: 2.0.0
- * ==============================================================================
- *                      Copyright (c) 2020 Charlie Projects                      
- *                      github.com/CharlieProjects/MULTI-19                      
- * ==============================================================================
- * ========================      CONVEYOR MULTI-19      =========================
- * ========================   Multiplo Colombia S.A.S.  =========================
- * ==============================================================================
- *  Description:
- *  This is the main code of the control structure of the conveyor MULTI-19 is 
- *  intend to be compiled whit Visual Studio Code using the Platformio plug, this
- *  code works by itself, including all the external libraries in the project path
- *  in the "lib" folder as well as the auxiliary functions and reusable resources
- *  in the "include" folder.
+#include <Arduino.h>
+
+
+
+void Automation();
+void Control(float ref);
+void AutomationOFF();
+
+struct GPIO_d {
+  uint8_t pin;
+  bool state;
+};
+
+
+/***
+ * Entradas y salidas del sistema
  * 
- *  The proposed programming architecture is based on object-oriented finite state 
- *  machines the architecture documentation can be found attached to the project 
- *  path, otherwise request directly from Charlie projects.
- * 
- * ==============================================================================
- *  MIT License
- *
- * Copyright (c) 2020 CHARLIE PROJECTS
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
-// #include <Config.h>
+***/
 
-#include <Conveyor.h>
-char incomingByte = 0;
+GPIO_d ON = {34, false};
+GPIO_d S = {12, false};
+GPIO_d m = {14, false};
+GPIO_d M = {27, false};
 
-void setup()
-{
-  Serial.begin(115200);
-  while (!Serial)
-    ;
-  Serial.println();
-  Serial.println("========== CHARLIE PROJECT ==========");
-  Serial.println("projectsbycharlie.com");
-  Serial.println("...Set Up...");
+GPIO_d S1 = {18, false};
+GPIO_d REVS = {4,true};
 
-  // display.init();
-  // displayBegin();
-  ConveyorBegin();
+void ON_I() {
+  if(digitalRead(ON.pin))
+  {
+    ON.state = true;
+    S1.state = true;
+  } 
+  else {
+    ON.state = false;
+    S1.state = false;
+  } 
 }
 
-void loop()
+void S_I() {
+  if(digitalRead(S.pin)) 
+  {
+    S.state = true;
+    REVS.state = false;
+  }
+  else{
+    S.state = false;
+    REVS.state = true;
+  }  
+}
+
+void m_I() {
+  if(digitalRead(m.pin)) m.state = true;
+  else m.state = false; 
+}
+
+void M_I() {
+  if(digitalRead(M.pin)) M.state = true;
+  else M.state = false; 
+}
+
+
+
+
+/***
+ * Control de la banda
+ * 
+***/
+
+float ts = 5;
+float VelMin = 75;
+float VelMax = 160;
+float u_apri = VelMin;
+float u_apos = VelMin;
+#define VelPWM 25
+
+
+
+void setup() {
+  
+  Serial.begin(115200);
+
+  pinMode(ON.pin,INPUT_PULLDOWN);
+  pinMode(S.pin,INPUT_PULLDOWN);
+  pinMode(m.pin,INPUT_PULLDOWN);
+  pinMode(M.pin,INPUT_PULLDOWN);
+  
+  attachInterrupt(ON.pin,ON_I,CHANGE);
+  attachInterrupt(S.pin,S_I,CHANGE);
+  attachInterrupt(m.pin,m_I,CHANGE);
+  attachInterrupt(M.pin,M_I,CHANGE);
+  
+  pinMode(S1.pin,OUTPUT);
+  digitalWrite(S1.pin,S1.state);
+  pinMode(REVS.pin,OUTPUT);
+  digitalWrite(REVS.pin,REVS.state);
+  
+  
+}
+
+void loop() {
+  if(ON.state) Automation();
+  else AutomationOFF();
+}
+
+void Automation() {
+  digitalWrite(S1.pin,S1.state);
+  if(S.state)
+  {
+    digitalWrite(REVS.pin, REVS.state);
+    dacWrite(VelPWM,ceil(VelMin));
+  }
+  else if(m.state) Control(-VelMax);
+  else if(M.state) Control(VelMax);
+
+}
+
+void Control(float ref) {
+  float Tau = ts / 5;
+  float delta = 0.01;
+  u_apri = u_apos;
+  u_apos = (ref - u_apri) * delta / Tau + u_apri;
+  u_apri = u_apos;
+  if(u_apos<0) 
+  {
+    REVS.state = true;
+    dacWrite(VelPWM, abs(ceil(-u_apos)));
+  }
+  else{
+    dacWrite(VelPWM, abs(ceil(u_apos)));
+    REVS.state = false;
+  } 
+  digitalWrite(REVS.pin,REVS.state);
+  delay(delta * 1000);
+}
+
+void AutomationOFF()
 {
-  switch (state)
-  {
-  case STAND_BY:
-    digitalWrite(POWER, LOW);
-    brakes();
-    batteryCheck();
-    dataLog();
-
-    state = (digitalRead(START) && !digitalRead(STOP) && !waitMillis) ? OPERATION : STAND_BY;
-    waitMillis = (state == OPERATION) ? millis() : !digitalRead(START) ? 0 : waitMillis;
-    break;
-  case OPERATION:
-    digitalWrite(POWER, HIGH);
-    elevation();
-    brakes();
-    batteryCheck();
-    illumination();
-    dataLog();
-
-    state = digitalRead(STOP) ? E_STOP : (!digitalRead(START) || (millis() - waitMillis > waitTime)) ? STAND_BY : OPERATION;
-    break;
-  case CHARGING:
-    digitalWrite(POWER, LOW);
-    batteryCheck();
-
-    state = !digitalRead(charger) ? STAND_BY : CHARGING;
-    break;
-  case E_STOP:
-    digitalWrite(POWER, LOW);
-    digitalWrite(FWR, LOW);
-    digitalWrite(RWD, LOW);
-    digitalWrite(up, LOW);
-    digitalWrite(down, LOW);
-    dataLog();
-
-    state = (!digitalRead(START) && !digitalRead(STOP)) ? STAND_BY : E_STOP;
-    break;
-  default:
-    Serial.println("Undefined state");
-
-    state = STAND_BY;
-    break;
-  }
-
-  if (oldstate != state)
-  {
-    oldstate = state;
-    Serial.print("STATE: ");
-    Serial.println(state);
-  }
-
-  if (Serial.available() > 0)
-  {
-    incomingByte = Serial.read();
-    Serial.print("I received: ");
-    Serial.println(incomingByte, DEC);
-  }
+  digitalWrite(S1.pin,S1.state);
+  u_apri = VelMin;
+  dacWrite(VelPWM, u_apri);
 }
